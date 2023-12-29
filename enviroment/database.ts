5ec1fe6504b01911b34b719e4d7125069485902d
redfortf <150962718+redfortf@users.noreply.github.com>
2023-12-29 03:47:20 +0300

    Make env-variables names consistent

--- enviroment/database.ts
+++ enviroment/database.ts
@@ -3,4 +3,4 @@ export const port      = Deno.env.get("SURREAL_PORT") ?? "8080";
 export const namespace = Deno.env.get("SURREAL_NAMESPACE") ?? "Learning";
 export const database  = Deno.env.get("SURREAL_DATABASE") ?? "Fresh";
 export const user      = Deno.env.get("SURREAL_USERNAME") ?? "root";
-export const pass      = Deno.env.get("SURREAL_DB_PASSWORD") ?? "root";
+export const pass      = Deno.env.get("SURREAL_PASSWORD") ?? "root";
